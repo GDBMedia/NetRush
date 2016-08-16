c4b312860a7c5c6845d0868054de5c15aca58206
Jeffrey Hackford <Jeff@netrush.com>
2016-08-16 14:24:24 -0700

    add overflow menu on ProductListActivity to allow user to logout and return to Login Activity

--- app/src/main/java/com/netrush/netrushapp/ui/LoginActivity.java
+++ app/src/main/java/com/netrush/netrushapp/ui/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,12 +23,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0);
+        Log.v("Test", pref.getString("Email", "fail"));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
         mLoginButton.setOnClickListener(this);
-        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0);
-//        SharedPreferences.Editor editor = pref.edit();
+//        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0);
         if (pref.getBoolean("LoggedIn", false)) {
             Intent intent = new Intent(LoginActivity.this, ProductListActivity.class);
             startActivity(intent);
@@ -44,7 +46,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             editor.putBoolean("LoggedIn", true);
             editor.putString("Email", Email);
             editor.putString("Password", Password);
-            editor.commit();
+            editor.apply();
 
             Intent intent = new Intent(LoginActivity.this, ProductListActivity.class);
             startActivity(intent);
--- app/src/main/java/com/netrush/netrushapp/ui/ProductListActivity.java
+++ app/src/main/java/com/netrush/netrushapp/ui/ProductListActivity.java
@@ -1,9 +1,13 @@
 package com.netrush.netrushapp.ui;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.netrush.netrushapp.R;
 
@@ -15,5 +19,28 @@ public class ProductListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_products);
         SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0);
         Log.v("Test", pref.getString("Email", "fail"));
+    }
+
+    @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.product_list_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.logout) {
+            SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0);
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putBoolean("LoggedIn", false);
+            editor.apply();
+
+            Intent intent = new Intent(ProductListActivity.this, LoginActivity.class);
+            startActivity(intent);
+//            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
