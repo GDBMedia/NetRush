8bae622c3ec00f836c085d32f0f76659cab36a5c
Garrett Biernat <laker77@gmail.com>
2016-08-20 00:26:28 -0700

    Removed was creds

--- app/src/main/java/com/netrush/netrushapp/Constants.java
+++ app/src/main/java/com/netrush/netrushapp/Constants.java
@@ -7,8 +7,6 @@ public class Constants {
 
     public static final String GETORDERS = "https://amazonorders.herokuapp.com/orders";
     public static final String ENDPOINT = "webservices.amazon.com";
-    public static final String AWS_ACCESS_KEY_ID = "AKIAJXLMRR36DJLF4CXA";
-    public static final String AWS_SECRET_KEY = "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";
-    public static final String ASSOCIATE_TAG = "netrush20-20";
+    
 
 }
