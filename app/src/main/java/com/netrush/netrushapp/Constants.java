c64cd66eada02d449331c249f395adc1a79cbe1b
Jeffrey Hackford <Jeff@netrush.com>
2016-08-29 11:18:36 -0700

    overflow menu icons

--- app/src/main/java/com/netrush/netrushapp/Constants.java
+++ app/src/main/java/com/netrush/netrushapp/Constants.java
@@ -5,12 +5,10 @@ package com.netrush.netrushapp;
  */
 public class Constants {
 
-    public static final String GETORDERS = "https://amazonorders.herokuapp.com/orders";
+    public static final String GETORDERS = "http://7f6ceb9d.ngrok.io/orders";
     public static final String TYPE = "type";
     public static final String AWS_ACCESS_KEY_ID = BuildConfig.AWS_ACCESS_KEY_ID;
     public static final String AWS_SECRET_KEY = BuildConfig.AWS_SECRET_KEY;
     public static final String ASSOCIATE_TAG = BuildConfig.ASSOCIATE_TAG;
     public static final String ENDPOINT = "webservices.amazon.com";
-
-
 }
